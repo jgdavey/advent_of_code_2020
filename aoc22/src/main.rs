3026930479382cfa46931c6a1e5921529fb5936e
Joshua Davey <josh@joshuadavey.com>
2020-12-24 13:22:13 -0500

    Day 22, Part 2

--- aoc22/src/main.rs
+++ aoc22/src/main.rs
@@ -1,18 +1,26 @@
-use std::collections::VecDeque;
+use std::collections::{HashSet, VecDeque};
 use std::str::FromStr;
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut game: Game = input.parse().unwrap();
-    game.play();
-    println!("Rounds: {}, Score: {}", game.round, game.score().unwrap());
+    println!("{}", game.summary());
+    let result = game.play();
+    println!("{}", game.summary());
+    println!(
+        "Result: {:?}, Rounds: {}, Score: {}",
+        result,
+        game.round,
+        game.score().unwrap()
+    );
 }
 
 #[derive(Debug)]
 struct Game {
-    round: usize,
     player1: VecDeque<usize>,
     player2: VecDeque<usize>,
+    round: usize,
+    seen: HashSet<(VecDeque<usize>, VecDeque<usize>)>,
 }
 
 impl FromStr for Game {
@@ -23,58 +31,134 @@ impl FromStr for Game {
                 .lines()
                 .skip(1)
                 .map(|l| l.parse().map_err(|_| "bad number"))
-                .collect::<Result<VecDeque<_>,_>>()
+                .collect::<Result<VecDeque<_>, _>>()
         });
         let player1 = players.next().ok_or("missing player")??;
         let player2 = players.next().ok_or("missing player")??;
-        Ok(Game {
-            round: 0,
-            player1,
-            player2,
-        })
+        Ok(Game::new(player1, player2))
     }
 }
 
+#[derive(Debug, PartialEq)]
+enum Player {
+    Player1,
+    Player2,
+}
+
 impl Game {
-    fn next_round(&mut self) -> Result<usize, usize> {
-        if self.player1.is_empty() || self.player2.is_empty() {
+    fn summary(&self) -> String {
+        format!(
+            "Game({}) after round {}:\nPlayer 1 {:?}\nPlayer 2 {:?}",
+            self.player1.len() + self.player2.len(),
+            self.round,
+            self.player1,
+            self.player2
+        )
+    }
+
+    fn new(player1: VecDeque<usize>, player2: VecDeque<usize>) -> Self {
+        //println!("p1: {}, p2: {}", player1.len(), player2.len());
+        Game {
+            player1,
+            player2,
+            round: 0,
+            seen: HashSet::new(),
+        }
+    }
+
+    fn game_over(&self) -> bool {
+        self.player1.is_empty() || self.player2.is_empty()
+    }
+
+    fn next_round(&mut self) -> Result<Player, usize> {
+        use Player::*;
+
+        if self.game_over() {
             return Err(self.round);
         }
-
-        let a = self.player1.pop_front().unwrap();
-        let b = self.player2.pop_front().unwrap();
-
-        if a > b {
-            self.player1.push_back(a);
-            self.player1.push_back(b);
-        } else {
-            self.player2.push_back(b);
-            self.player2.push_back(a);
-        }
-
         self.round += 1;
 
-        Ok(self.round)
+        let state = (self.player1.clone(), self.player2.clone());
+
+        if self.seen.contains(&state) || self.round > 1000 {
+            self.player2.clear();
+            return Ok(Player1);
+        }
+
+        self.seen.insert(state);
+
+        let p1 = self.player1.pop_front().unwrap();
+        let p2 = self.player2.pop_front().unwrap();
+
+        let (player, cards) = if self.player1.len() >= p1 && self.player2.len() >= p2 {
+            let mut subgame = Game::new(
+                self.player1
+                    .iter()
+                    .take(p1)
+                    .cloned()
+                    .collect::<VecDeque<_>>(),
+                self.player2
+                    .iter()
+                    .take(p2)
+                    .cloned()
+                    .collect::<VecDeque<_>>(),
+            );
+            let result = subgame.play();
+            match result {
+                None => panic!("None result"),
+                Some(Player2) => (Player2, vec![p2, p1]),
+                Some(Player1) => (Player1, vec![p1, p2]),
+            }
+        } else {
+            if p1 > p2 {
+                (Player1, vec![p1, p2])
+            } else {
+                (Player2, vec![p2, p1])
+            }
+        };
+
+        let p = if let Player1 = player {
+            &mut self.player1
+        } else {
+            &mut self.player2
+        };
+
+        for &card in &cards {
+            p.push_back(card)
+        }
+
+        Ok(player)
     }
 
     fn score(&self) -> Option<usize> {
-        let winner = if self.player1.is_empty() {
-            &self.player2
-        } else if self.player2.is_empty() {
+        let winner = if self.player2.is_empty() {
             &self.player1
+        } else if self.player1.is_empty() {
+            &self.player2
         } else {
             return None;
         };
 
-        Some(winner.iter().rev().enumerate().map(|(i, val)| (i + 1) * val).sum())
+        Some(
+            winner
+                .iter()
+                .rev()
+                .enumerate()
+                .map(|(i, val)| (i + 1) * val)
+                .sum(),
+        )
     }
 
-    fn play(&mut self) -> usize {
-        while let Ok(_) = self.next_round() {}
-        self.score().unwrap()
+    fn play(&mut self) -> Option<Player> {
+        let mut round = self.next_round().ok()?;
+        while let Ok(r) = self.next_round() {
+            round = r
+        }
+        Some(round)
     }
 }
 
+#[allow(unused_must_use)]
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -86,8 +170,8 @@ mod tests {
                      Player 2:\n5\n8\n4\n7\n10";
 
         let game: Game = input.parse().unwrap();
-        assert_eq!(game.player1, VecDeque::from(vec![9,2,6,3,1]));
-        assert_eq!(game.player2, VecDeque::from(vec![5,8,4,7,10]));
+        assert_eq!(game.player1, VecDeque::from(vec![9, 2, 6, 3, 1]));
+        assert_eq!(game.player2, VecDeque::from(vec![5, 8, 4, 7, 10]));
     }
 
     #[test]
@@ -97,12 +181,41 @@ mod tests {
                      Player 2:\n5\n8\n4\n7\n10";
 
         let mut game: Game = input.parse().unwrap();
-        assert_eq!(game.next_round(), Ok(1));
-        assert_eq!(game.player1, VecDeque::from(vec![2,6,3,1,9,5]));
-        assert_eq!(game.player2, VecDeque::from(vec![8,4,7,10]));
+        game.next_round();
 
-        game.play();
-        assert_eq!(game.round, 29);
-        assert_eq!(game.score(), Some(306))
+        assert_eq!(game.player1, VecDeque::from(vec![2, 6, 3, 1, 9, 5]));
+        assert_eq!(game.player2, VecDeque::from(vec![8, 4, 7, 10]));
+
+        assert_eq!(game.play(), Some(Player::Player2));
+        assert_eq!(game.round, 17);
+        assert_eq!(game.score(), Some(291));
+
+        let mut subgame = Game::new(
+            VecDeque::from(vec![9, 8, 5, 2]),
+            VecDeque::from(vec![10, 1, 7]),
+        );
+
+        assert_eq!(subgame.play(), Some(Player::Player2));
+        assert_eq!(subgame.round, 6);
+    }
+
+    #[test]
+    fn test_no_infinite() {
+        let input = "Player 1:\n\
+                     43\n\
+                     19\n\
+                     \n\
+                     Player 2:\n\
+                     2\n\
+                     29\n\
+                     14";
+        let mut game: Game = input.parse().unwrap();
+        for _ in 0..10 {
+            let _ = game.next_round();
+        }
+
+        println!("Result: {:?}", game.play());
+        println!("Game: {:?}", game);
+        assert_eq!(game.player2, VecDeque::from(vec![]));
     }
 }
