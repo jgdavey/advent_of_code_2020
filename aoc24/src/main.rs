032d43d5fba6aba43f8cea233591d33335bb255b
Joshua Davey <josh@joshuadavey.com>
2020-12-27 14:20:29 -0500

    Day 24, Part 2

--- aoc24/src/main.rs
+++ aoc24/src/main.rs
@@ -4,8 +4,12 @@ use std::str::{Chars, FromStr};
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let pattern: Pattern = input.parse().unwrap();
-    let floor = pattern.floor();
-    println!("Flipped: {}", floor.flipped_count());
+    let mut floor = pattern.floor();
+    println!("Day {}, Flipped: {}", floor.day, floor.flipped_count());
+    while floor.day < 100 {
+        floor.tick();
+    }
+    println!("Day {}, Flipped: {}", floor.day, floor.flipped_count());
 }
 
 struct Pattern {
@@ -22,9 +26,12 @@ impl Pattern {
                 flipped.insert(coord);
             }
         }
-        Floor { flipped }
+        Floor { day: 0, flipped }
     }
 }
+
+// Using an Axial coordinate system, as described here:
+// https://www.redblobgames.com/grids/hexagons/
 
 // r: row, identical a standard "y" value, axis like |
 // q: "column", on a slant, axis like /
@@ -35,7 +42,9 @@ struct Path(Vec<Coord>);
 
 impl Path {
     fn coordinate(&self) -> Coord {
-        self.0.iter().fold((0,0), |(q, r), (dq, dr)| (q + dq, r + dr))
+        self.0
+            .iter()
+            .fold((0, 0), |(q, r), (dq, dr)| (q + dq, r + dr))
     }
 }
 
@@ -77,12 +86,44 @@ impl FromStr for Pattern {
 }
 
 struct Floor {
-    flipped: HashSet<Coord>
+    day: usize,
+    flipped: HashSet<Coord>,
+}
+
+fn hex_neighbors((q, r): &Coord) -> HashSet<Coord> {
+    [(0, -1), (1, -1), (1, 0), (0, 1), (-1, 1), (-1, 0)]
+        .iter()
+        .map(|(dq, dr)| (q + dq, r + dr))
+        .collect()
 }
 
 impl Floor {
     fn flipped_count(&self) -> usize {
         self.flipped.len()
+    }
+
+    fn tick(&mut self) {
+        let mut check = self.flipped.clone();
+        for coord in self.flipped.iter() {
+            check.extend(hex_neighbors(coord));
+        }
+
+        let mut new_flipped = self.flipped.clone();
+
+        for coord in check.iter() {
+            let flipped_neighbors = self.flipped.intersection(&hex_neighbors(coord)).count();
+
+            if self.flipped.contains(coord) {
+                if flipped_neighbors == 0 || flipped_neighbors > 2 {
+                    new_flipped.remove(coord);
+                }
+            } else if flipped_neighbors == 2 {
+                new_flipped.insert(*coord);
+            }
+        }
+
+        self.flipped = new_flipped;
+        self.day += 1;
     }
 }
 
@@ -114,6 +155,23 @@ mod tests {
                      wseweeenwnesenwwwswnew";
         let pattern: Pattern = input.parse().unwrap();
         assert_eq!(pattern.paths[2].coordinate(), (-3, 3));
-        assert_eq!(pattern.floor().flipped_count(), 10);
+        let mut floor = pattern.floor();
+
+        println!("Neighbors of {:?}: {:?}", (2, -1), hex_neighbors(&(2, -1)));
+        assert_eq!(floor.flipped_count(), 10);
+
+        floor.tick();
+        assert_eq!(floor.flipped_count(), 15);
+        assert_eq!(floor.day, 1);
+        floor.tick();
+        assert_eq!(floor.flipped_count(), 12);
+        assert_eq!(floor.day, 2);
+        floor.tick();
+        assert_eq!(floor.flipped_count(), 25);
+
+        while floor.day < 100 {
+            floor.tick();
+        }
+        assert_eq!(floor.flipped_count(), 2208);
     }
 }
